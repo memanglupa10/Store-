cfadd24c93161961708fe56e26c6894d85b8e77b
Ade Triyanto <ade.triyanto@modena.com>
2026-08-05 18:43:39 +0700

    fix: loadDB initialization in Midtrans webhook handler v41

--- server.js
+++ server.js
@@ -1025,6 +1025,7 @@ async function handleRequest(req, res) {
       }
     }
 
+    const db = loadDB();
     let order = db.orders.find(o => o.id === targetOrderId || o.payment_reference === targetOrderId || (o.qris_info && o.qris_info.mayar_id === targetOrderId));
 
     if (!order) {
